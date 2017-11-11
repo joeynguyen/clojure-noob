71af2e15001cfad8cb4c8355f8280b6740b594cd
Joey Nguyen <jnn193@gmail.com>
2017-11-11 00:28:17 -0600

    Add new internal function

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,13 +1,12 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn greeting
+  [name]
+  (str "Hello " name))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I'm a little teapot!"))
-
-; (defn greeting
-;   [name]
-;   (str "Hello " name))
-
-; greeting("Joey")
+  (do (println "I'm a little teapot!")
+    (println (greeting "Joey"))))
