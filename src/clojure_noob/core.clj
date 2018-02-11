8548630a3a8732cca142c6aa860eea00eb9024ef
Joey Nguyen <jnn193@gmail.com>
2018-02-11 05:24:21 -0600

    Learned take, drop and take/drop-while

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -197,3 +197,25 @@
   (do
     (println "I'm a little teapot!")
     (println "Hi there!")))
+
+(take 3 [1 2 3 4 5 6 7 8 9 10])
+(drop 3 [1 2 3 4 5 6 7 8 9 10])
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+    {:month 1 :day 2 :human 5.1 :critter 2.0}
+    {:month 2 :day 1 :human 4.9 :critter 2.1}
+    {:month 2 :day 2 :human 5.0 :critter 2.5}
+    {:month 3 :day 1 :human 4.2 :critter 3.3}
+    {:month 3 :day 2 :human 4.0 :critter 3.8}
+    {:month 4 :day 1 :human 3.7 :critter 3.9}
+    {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+; get everything before March
+(take-while #(< (:month %) 3) food-journal)
+; get everything after February
+(drop-while #(< (:month %) 3) food-journal)
+
+; get just February
+(take-while #(< (:month %) 3)
+  (drop-while #(< (:month %) 2) food-journal))
