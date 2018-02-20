73effcea7385ddaaaf3bf227f59738f6276e8214
Joey Nguyen <jnn193@gmail.com>
2018-02-20 02:12:39 -0600

    Learned lazy seqs and infinite seqs

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -242,3 +242,44 @@
 
 ; `concat` appends the members of one sequence to the end of another
 (concat [1 2] [3 4])
+
+; lazy seqs
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true  :name "McFishwich"}
+    1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+    2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+    3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+    (not (:has-pulse? record))
+      record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+    (map vampire-related-details social-security-numbers))))
+
+(time (vampire-related-details 0))
+
+(time (def mapped-details (map vampire-related-details (range 0 1000000))))
+
+(time (first mapped-details))
+
+; infinite seqs
+(concat (take 8 (repeat "na")) ["Batman!"])
+
+(take 3 (repeatedly (fn [] (rand-int 10))))
+
+
+
+
+
+
+
